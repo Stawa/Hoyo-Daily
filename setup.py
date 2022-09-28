c6cda9cc125eccf0d1576abeb1afbe9ed4b05199
Stawa <69102292+Stawa@users.noreply.github.com>
2022-09-28 13:25:54 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name="Hoyo-Daily",
     version="1.0.2",
     packages=find_packages(),
-    install_requires=["genshin"],
+    install_requires=["genshin", "discord-webhook", "pytz", "requests"],
     license="MIT",
     url="https://github.com/Stawa/Hoyo-Daily",
     project_urls={
@@ -18,12 +18,7 @@ setup(
     description="Automatic HoyoLab Daily Check-in with Python",
     long_description=README,
     long_description_content_type="text/markdown",
-    keywords=[
-        "hoyolab",
-        "genshin impact",
-        "honkai3rd",
-        "daily-checkin"
-    ],
+    keywords=["hoyolab", "genshin impact", "honkai3rd", "daily-checkin"],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3",
