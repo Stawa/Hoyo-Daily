ac8896598c9e0265af94fa80313cd74401e1e6a5
Stawa <69102292+Stawa@users.noreply.github.com>
2022-09-28 22:00:17 +0800

    Update redeem.py

--- hoyo_daily/redeem.py
+++ hoyo_daily/redeem.py
@@ -1,6 +1,8 @@
+import requests
 import genshin
 import asyncio
 import pytz
+from genshin import Game
 from discord_webhook import DiscordWebhook, DiscordEmbed
 from datetime import datetime as dt
 from typing import Optional
@@ -14,6 +16,7 @@ class AutoRedeem:
         webhook_url: str = None,
         cookies: dict = None,
     ) -> None:
+        self.code_url = "https://hoyo-daily.vercel.app/api/v1/codes/"
         self.url = url
         self.authkey = (
             authkey if (authkey) else genshin.utility.extract_authkey(self.url)
@@ -24,15 +27,40 @@ class AutoRedeem:
             cookies={
                 "ltuid": int(cookies.get("ltuid")),
                 "ltoken": cookies.get("ltoken"),
+                "account_id": int(cookies.get("ltuid")),
+                "cookie_token": cookies.get("cookie_token")
+                if cookies.get("cookie_token")
+                else None,
             },
             authkey=self.authkey,
         )
 
     async def auto_redeem(self) -> None:
         try:
-            x = await self.Client.redeem_code(
-                code="GENSHINGIFT", uid=await self.Client._get_uid(game="genshin")
+            codes = requests.get(self.code_url).json().get("obtainable")["codes"]
+            webhook = DiscordWebhook(url=self.webook_url)
+            account = await self.Client.genshin_accounts()
+
+            for i in range(len(codes)):
+                await self.Client.redeem_code(
+                    code=codes[i], uid=await self.Client._get_uid(game=Game.GENSHIN)
+                )
+                await asyncio.sleep(5)
+
+            embed = DiscordEmbed(
+                title="Genshin Impact Redeem Code",
+                description=f"━━━━━━━━━━━━━━━━━━━━━━━━━\nUsername: {account[0].nickname} [{account[0].server_name}] ({await self.Client._get_uid(game='genshin')})\nRedeemed Codes: {', '.join(codes)}\n━━━━━━━━━━━━━━━━━━━━━━━━━",
+                color="a2006d",
             )
-            return x
+            embed.set_timestamp()
+            embed.set_thumbnail(
+                url="https://upload-static.hoyoverse.com/event/2021/02/25/f4450e0ef470f777fca0b3dd95813734_1653002626503274756.png"
+            )
+            embed.set_footer(text="Developed by Stawa")
+
+            webhook.add_embed(embed=embed)
+            webhook.execute()
+
+            return codes
         except (genshin.RedemptionException, genshin.InvalidCookies) as e:
             return e
